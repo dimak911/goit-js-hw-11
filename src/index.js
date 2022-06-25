3627e58bb60d9d4eb2f9fdcb00de6154f8879f2b
Dmytro Kniahnytskyi <dimak911@gmail.com>
2022-06-25 16:51:38 +0300

    11-01

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { getImage } from './js/fetch-img.js';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   form: document.querySelector('#search-form'),
   gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
+let currentQuery = '';
+const lightbox = new SimpleLightbox('.gallery a');
 
 refs.form.addEventListener('submit', searchImg);
+refs.loadMoreBtn.addEventListener('click', loadAndRenderImgs);
 
 function searchImg(event) {
   event.preventDefault();
   refs.gallery.innerHTML = '';
 
   const { searchQuery } = event.target.elements;
-  const MatchedImages = getImage(searchQuery.value);
-
-  MatchedImages.then(images => {
-    // console.log(images);
-
-    const galleryMarkup = images.map(img => makeImgCard(img)).join('');
-    makeGallery(galleryMarkup);
-  });
+  currentQuery = searchQuery.value;
+  if (currentQuery) {
+    loadAndRenderImgs();
+  } else {
+    hideLoadMoreBtn();
+    Notify.info('Enter a query');
+  }
 }
 
 function makeImgCard({
@@ -34,6 +38,7 @@ function makeImgCard({
   downloads,
 }) {
   return `<div class="photo-card">
+            <div class="photo-card-img-wrapper">
             <a href="${largeImageURL}">
               <img
                 class="photo-card-img"
@@ -42,6 +47,7 @@ function makeImgCard({
                 loading="lazy"
               />
             </a>
+            </div>
               <div class="info">
                 <p class="info-item">
                   <span><b>Likes</b></span>
@@ -64,6 +70,37 @@ function makeImgCard({
 }
 
 function makeGallery(images) {
-  refs.gallery.innerHTML = images;
-  const lightbox = new SimpleLightbox('.gallery a');
+  refs.gallery.insertAdjacentHTML('beforeend', images);
+  lightbox.refresh();
+}
+
+function loadAndRenderImgs() {
+  const MatchedImages = getImage(currentQuery);
+
+  hideLoadMoreBtn();
+
+  MatchedImages.then(images => {
+    if (images === 'end') {
+      hideLoadMoreBtn();
+      Notify.info("We're sorry, but you've reached the end of search results.");
+
+      return;
+    }
+
+    if (images) {
+      showLoadMoreBtn();
+      const galleryMarkup = images.map(img => makeImgCard(img)).join('');
+      makeGallery(galleryMarkup);
+    } else {
+      return;
+    }
+  }).catch(error => console.log(error));
+}
+
+function hideLoadMoreBtn() {
+  refs.loadMoreBtn.classList.add('is-hidden');
+}
+
+function showLoadMoreBtn() {
+  refs.loadMoreBtn.classList.remove('is-hidden');
 }
--- src/js/fetch-img.js
+++ src/js/fetch-img.js
@@ -1,28 +1,46 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/';
+let page = 1;
+let currentQuery = '';
 
 export async function getImage(query) {
   try {
+    if (currentQuery !== query) {
+      page = 1;
+    }
+
+    currentQuery = query;
     const response = await axios.get(`${BASE_URL}api/`, {
       params: {
         key: '28213280-eb811a12977e7e61e372ebe76',
-        q: query,
+        q: currentQuery,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: 'true',
+        page,
+        per_page: 40,
       },
     });
 
     const totalHits = response.data.totalHits;
 
+    if (page * response.data.hits.length > totalHits) {
+      return 'end';
+    }
+
     if (totalHits > 0) {
-      successAlert(totalHits);
+      if (page === 1) {
+        successAlert(totalHits);
+      }
+      page += 1;
+
       return response.data.hits;
     } else {
       throw new Error();
     }
-  } catch {
+  } catch (error) {
+    console.log(error);
     Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
